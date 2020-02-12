207d3c3453342f363f4c89630f8ffaa55b693f2a
Wojciech Wypior <w.wypior@f5.com>
2020-02-12 17:12:40 +0100

    added f5 cloud services httpapi plugin added beacon token module to manage beacon tokens

---
+++ library/module_utils/common.py
@@ -0,0 +1,83 @@
+
+
+from ansible.module_utils.six import iteritems
+from ansible.module_utils.parsing.convert_bool import BOOLEANS_TRUE
+from ansible.module_utils.parsing.convert_bool import BOOLEANS_FALSE
+from collections import defaultdict
+
+
+def is_empty_list(seq):
+    if len(seq) == 1:
+        if seq[0] == '' or seq[0] == 'none':
+            return True
+    return False
+
+
+def flatten_boolean(value):
+    truthy = list(BOOLEANS_TRUE) + ['enabled', 'True', 'true']
+    falsey = list(BOOLEANS_FALSE) + ['disabled', 'False', 'false']
+    if value is None:
+        return None
+    elif value in truthy:
+        return 'yes'
+    elif value in falsey:
+        return 'no'
+
+
+class AnsibleF5Parameters(object):
+    def __init__(self, *args, **kwargs):
+        self._values = defaultdict(lambda: None)
+        self._values['__warnings'] = []
+        self._module = kwargs.pop('module', None)
+        self._params = {}
+
+        params = kwargs.pop('params', None)
+        if params:
+            self.update(params=params)
+            self._params.update(params)
+
+    def update(self, params=None):
+        if params:
+            self._params.update(params)
+            for k, v in iteritems(params):
+                if self.api_map is not None and k in self.api_map:
+                    map_key = self.api_map[k]
+                else:
+                    map_key = k
+                # Handle weird API parameters like `dns.proxy.__iter__` by
+                # using a map provided by the module developer
+                class_attr = getattr(type(self), map_key, None)
+                if isinstance(class_attr, property):
+                    # There is a mapped value for the api_map key
+                    if class_attr.fset is None:
+                        # If the mapped value does not have
+                        # an associated setter
+                        self._values[map_key] = v
+                    else:
+                        # The mapped value has a setter
+                        setattr(self, map_key, v)
+                else:
+                    # If the mapped value is not a @property
+                    self._values[map_key] = v
+
+    def api_params(self):
+        result = {}
+        for api_attribute in self.api_attributes:
+            if self.api_map is not None and api_attribute in self.api_map:
+                result[api_attribute] = getattr(self, self.api_map[api_attribute])
+            else:
+                result[api_attribute] = getattr(self, api_attribute)
+        result = self._filter_params(result)
+        return result
+
+    def __getattr__(self, item):
+        # Ensures that properties that weren't defined, and therefore stashed
+        # in the `_values` dict, will be retrievable.
+        return self._values[item]
+
+    def _filter_params(self, params):
+        return dict((k, v) for k, v in iteritems(params) if v is not None)
+
+
+class F5CollectionError(Exception):
+    pass
---
+++ library/modules/beacon_tokens.py
@@ -0,0 +1,264 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright: (c) 2020, F5 Networks Inc.
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'certified'}
+
+DOCUMENTATION = r'''
+---
+module: Manage Beacon tokens on F5 Cloud Services
+short_description: Manage Beacon tokens on F5 Cloud Services
+description:
+  - Manage Beacon tokens on F5 Cloud Services.
+version_added: "f5_beacon 1.0"
+options:
+  name:
+    description:
+      - Specifies the name of the Beacon token to manage/create.
+    type: str
+    required: True
+  description:
+    description:
+      - User created token description.
+    type: str
+author:
+  - Wojciech Wypior (@wojtek0806)
+'''
+
+EXAMPLES = r'''
+- name: Create Beacon Token with description
+  beacon_tokens:
+    name: "foobar"
+    description: "Created by Ansible tool"
+    state: present
+
+- name: Delete Beacon Token
+  beacon_tokens:
+    name: "foobar"
+    state: absent
+'''
+
+RETURN = r'''
+name:
+  description: The name of the created token.
+  returned: changed
+  type: str
+  sample: Token_foo
+description:
+  description: The new description of the token.
+  returned: changed
+  type: str
+  sample: My Token
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+
+try:
+    from library.module_utils.common import AnsibleF5Parameters
+    from library.module_utils.common import F5CollectionError
+except ImportError:
+    from ansible_collections.f5networks.f5_beacon.plugins.module_utils import AnsibleF5Parameters
+    from ansible_collections.f5networks.f5_beacon.plugins.module_utils import F5CollectionError
+
+
+class Parameters(AnsibleF5Parameters):
+    api_map = {
+    }
+
+    api_attributes = [
+        'name',
+        'description',
+    ]
+
+    returnables = [
+        'name',
+        'description',
+    ]
+
+
+class ModuleParameters(Parameters):
+    pass
+
+
+class Changes(Parameters):
+    def to_return(self):
+        result = {}
+        try:
+            for returnable in self.returnables:
+                change = getattr(self, returnable)
+                if isinstance(change, dict):
+                    result.update(change)
+                else:
+                    result[returnable] = change
+            result = self._filter_params(result)
+        except Exception:
+            pass
+        return result
+
+
+class UsableChanges(Changes):
+    pass
+
+
+class ReportableChanges(Changes):
+    pass
+
+
+class Difference(object):
+    def __init__(self, want, have=None):
+        self.want = want
+        self.have = have
+
+    def compare(self, param):
+        try:
+            result = getattr(self, param)
+            return result
+        except AttributeError:
+            return self.__default(param)
+
+    def __default(self, param):
+        attr1 = getattr(self.want, param)
+        try:
+            attr2 = getattr(self.have, param)
+            if attr1 != attr2:
+                return attr1
+        except AttributeError:
+            return attr1
+
+
+class ModuleManager(object):
+    def __init__(self, *args, **kwargs):
+        self.module = kwargs.pop('module', None)
+        self.client = kwargs.pop('client', None)
+        self.url = '/beacon/v1/telemetry-token'
+        self.want = ModuleParameters(params=self.module.params)
+        self.changes = UsableChanges()
+
+    def _announce_deprecations(self, result):
+        warnings = result.pop('__warnings', [])
+        for warning in warnings:
+            self.module.deprecate(
+                msg=warning['msg'],
+                version=warning['version']
+            )
+
+    def _set_changed_options(self):
+        changed = {}
+        for key in Parameters.returnables:
+            if getattr(self.want, key) is not None:
+                changed[key] = getattr(self.want, key)
+        if changed:
+            self.changes = UsableChanges(params=changed)
+
+        return False
+
+    def exec_module(self):
+        changed = False
+        result = dict()
+        state = self.want.state
+
+        if state == "present":
+            changed = self.present()
+        elif state == "absent":
+            changed = self.absent()
+
+        reportable = ReportableChanges(params=self.changes.to_return())
+        changes = reportable.to_return()
+        result.update(**changes)
+        result.update(dict(changed=changed))
+        self._announce_deprecations(result)
+        return result
+
+    def present(self):
+        if self.exists():
+            return False
+        else:
+            return self.create()
+
+    def absent(self):
+        if self.exists():
+            return self.remove()
+        return False
+
+    def remove(self):
+        if self.module.check_mode:
+            return True
+        self.remove_from_device()
+        if self.exists():
+            raise F5CollectionError("Failed to delete the resource.")
+        return True
+
+    def create(self):
+        self._set_changed_options()
+        if self.module.check_mode:
+            return True
+        self.create_on_device()
+        return True
+
+    def exists(self):
+        response = self.client.get(self.url + '/' + self.want.name)
+        if response['code'] == 404:
+            return False
+        elif response['code'] == 200:
+            return True
+        else:
+            raise F5CollectionError(response['contents'])
+
+    def create_on_device(self):
+        params = self.changes.api_params()
+        response = self.client.post(self.url, data=params)
+        if response['code'] == 200:
+            return True
+        else:
+            raise F5CollectionError(response['code'], response['contents'])
+
+    def remove_from_device(self):
+        response = self.client.delete(self.url + '/' + self.want.name)
+        if response['code'] == 200:
+            return True
+        else:
+            raise F5CollectionError(response['code'], response['contents'])
+
+
+class ArgumentSpec(object):
+    def __init__(self):
+        self.supports_check_mode = True
+        argument_spec = dict(
+            name=dict(required=True),
+            description=dict(),
+            state=dict(
+                default='present',
+                choices=['present', 'absent']
+            ),
+        )
+        self.argument_spec = {}
+        self.argument_spec.update(argument_spec)
+
+
+def main():
+    spec = ArgumentSpec()
+
+    module = AnsibleModule(
+        argument_spec=spec.argument_spec,
+        supports_check_mode=spec.supports_check_mode,
+    )
+
+    try:
+        mm = ModuleManager(module=module, client=Connection(module._socket_path))
+        results = mm.exec_module()
+        module.exit_json(**results)
+    except F5CollectionError as ex:
+        module.fail_json(msg=str(ex))
+
+
+if __name__ == '__main__':
+    main()
